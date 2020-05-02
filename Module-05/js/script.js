9f4ea77374830d19fbc9dd3d0c8c98fda2a5e8df
sgalvis1 <sgalvis1@hotmail.com>
2020-05-02 16:33:22 +0200

    Update color function

--- Module-05/js/script.js
+++ Module-05/js/script.js
@@ -4,9 +4,9 @@
 // });
 
 var bton = document.getElementById("btn");
-bton.addEventListener("click",sayIt);
+bton.addEventListener("click", sayIt);
 
-bton.addEventListener("blur", function(){
+bton.addEventListener("blur", function () {
     this.textContent = "Say it";
 });
 
@@ -21,20 +21,20 @@ function sayIt() {
     var msg = "Hello " + input + " !";
     var msg2 = "<h2> Hello " + input + " ! <h2>";
 
-    
+
 
     output.className = "message";
-    output.style.backgroundColor="#000000";
-    output.style.color="#ffffff";
+    output.style.backgroundColor = "#000000";
+    output.style.color = "#ffffff";
     output.style.textAlign = "center";
-    
+
     output.textContent = msg;
     output.innerHTML = msg2;
-    
-    if(input==="student"){
+
+    if (input === "student") {
         var title = document.querySelector("#title");
         title.textContent = title.textContent + " !!!";
-        
+
     }
 }
 
@@ -45,23 +45,38 @@ screenLog.addEventListener('mousemove', logKey);
 // Map values
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-  };
+};
 
-  // Change color Function
-function logKey(e) {
-    // var map = (0,0, 10, -50, 50);
-    var xmap = scale(e.screenX,398,728,0,255);
-    var ymap = scale(e.screenY,177,373,0,255);
-
-//   screenLog.innerText = `
-//     Screen X/Y: ${e.screenX}, ${e.screenY}
-//     Client X/Y: ${e.clientX}, ${e.clientY}
-//     Map X/Y: ${xmap}, ${ymap}`;
-    screenLog.style.backgroundColor = `rgb(${xmap},${ymap}, ${xmap})`;
+var test = (msg)=>{
+    console.log(msg);
 }
 
-document.querySelector("body").addEventListener("mousemove", function(event){
-    if(event.shiftKey === true){
-        console.log("x: " + event.clientX);
+// Change color Function
+function logKey(e) {
+    // var map = (0,0, 10, -50, 50);
+    let rect = e.target.getBoundingClientRect();
+
+    let xA = e.clientX - rect.x;
+    let yA = e.clientY - rect.y;
+    var xmap = scale(e.clientX, rect.left, rect.right, 0, 255);
+    var ymap = scale(e.clientY, rect.top, rect.bottom, 0, 255);
+
+
+    screenLog.style.backgroundColor = `rgb(${xmap},${ymap}, 100)`;
+    screenLog.style.borderColor = `rgb(${xmap},${ymap}, 100)`;
+    
+    if (e.shiftKey === true) {
+        screenLog.innerText = `
+            Screen X/Y: ${e.screenX}, ${e.screenY}
+            Client X/Y: ${e.clientX}, ${e.clientY}
+            Absolute X|Y : ${xA}, ${yA} 
+            Map X/Y: ${xmap}, ${ymap}`;
+        // console.log(e.target);
+    }
+}
+
+document.querySelector("body").addEventListener("mousemove", function (event) {
+    if (event.shiftKey === true) {
+        console.log("x: " + event.clientX + " y: " + event.clientY);
     };
 });
