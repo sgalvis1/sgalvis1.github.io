c49b24290e6965bf1d108ed0f62155d994fe8b62
sgalvis1 <sgalvis1@hotmail.com>
2020-05-07 18:42:59 +0200

    Dynamic Home Page and Preload Image

---
+++ Chinse Restaurant/js/ajax-utils.js
@@ -0,0 +1,46 @@
+(function (global){
+    var ajaxUtils= {};
+    // Create the XHMLHttp object if it's supported
+    function getRequestObject(){
+        if (window.XMLHttpRequest){
+            return (new XMLHttpRequest());
+        }else{
+            global.alert("Ajax is not supported");
+            return null;
+        }
+    }
+    
+    // Get data from server function
+    ajaxUtils.sendGetRequest = function(requestURL, responseHandler,isJsonResponse){
+        console.log("AJAX Function");
+        var request = getRequestObject();
+        // If ready run the handle function
+        request.onreadystatechange = function(){
+            // Test if the message is ready and run the handle function
+            handleResponse(request,responseHandler,isJsonResponse);
+        };
+        //Get and send the object to java engine
+        request.open("GET", requestURL, true);
+        request.send("null");
+    };
+
+    // Handle function
+    function handleResponse (request,responseHandler,isJsonResponse){
+        // console.log(request.status);
+        // Test if the message is ready and run the function
+        if( (request.readyState == 4) && (request.status == 200)){
+            // Run the handler passed function
+            if(isJsonResponse == undefined){
+                isJsonResponse = true;
+            }
+            if(isJsonResponse){
+                responseHandler(JSON.parse(request.responseText));
+            }else {
+                responseHandler(request.responseText);
+            }
+        }
+    }
+
+    global.$ajaxUtils = ajaxUtils;
+
+})(window);
--- Chinse Restaurant/js/script.js
+++ Chinse Restaurant/js/script.js
@@ -1,27 +1,67 @@
 //Using jquery
 
-function showLoader() {
-    $("body").addClass('preloader');
-    setTimeout(showPage, 2000);
-    function showPage() {
-        // document.getElementById("myLoader").style.display ="none";
-        document.getElementById("myLoader").style.position ="relative";
-        // document.getElementById("bodyContent").style.display = "block";
-        $("#myLoader").fadeOut(500);
-        $("#bodyContent").fadeIn(2000);
-        $("body").removeClass('preloader');
-    }
-}
-
+// Menu blur function
 $(function () { // Same as document.addEventListener("DOMContentLoaded")
 
     $("#navBarToggle").blur(function (event) {
         var screenWidth = window.innerWidth;
-         function closeBar(){
+        function closeBar() {
             if (screenWidth < 769) {
                 $("#navbarNav").collapse('hide');
             }
         };
-        setTimeout(closeBar,500);
+        setTimeout(closeBar, 500);
     });
 });
+
+// Sigle page content manager  
+(function (global) {
+    // Function object 
+    var yu = {};
+
+    // Variables 
+    var homeHtml = "snippets/home-snippets.html";
+
+    // InnerHTML custom function 
+    var insertHTML = function (selector, html) {
+        var targetElem = $(selector);
+        targetElem.html(html);
+    };
+
+    // Show loading page
+    var showLoading = function (selector) {
+        var html = "<div class ='text-center'>";
+        html += "<img src='img/25.gif'></div>";
+        insertHTML(selector, html);
+    };
+
+    //On page load (Before images or css)
+    document.addEventListener("DOMContentLoaded", function (event) {
+        showLoader();
+        //On first load show home view
+        showLoading("#main-content");
+        $ajaxUtils.sendGetRequest(homeHtml, function (responseText) {
+            $("#main-content").html(responseText);
+        }, false);
+    });
+    global.$yu = yu;
+
+})(window);
+
+
+function showLoader() {
+    // var body = document.getElementById("globalBody");
+    // body.onload = ()=>{console.log("Hello")};
+    // $("body").addClass('preloader');
+    setTimeout(showPage, 2000);
+    function showPage() {
+        document.getElementById("myLoader").style.display = "none";
+        document.getElementById("myLoader").style.position = "block";
+        // document.getElementById("bodyContent").style.display = "block";
+        // $("#myLoader").fadeOut(500);
+        $("#bodyContent").fadeIn(2000);
+        $("body").addClass('preloader');
+    }
+}
+
+
